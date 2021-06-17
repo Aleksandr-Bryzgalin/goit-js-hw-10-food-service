57c54eac22db4de822fcd7b00decacd610107d2f
Aleksandr-Bryzgalin <aleksandr.bryzgalin@hotmail.com>
2021-06-17 14:16:50 +0300

    добавляет разметку

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 import './sass/main.scss';
+
+import menuCardsTpl from './templates/menu-card-markup.hbs';
+import items from './menu.json';
+
+
+const menuCont = document.querySelector('.js-menu');
+const menuMarkup = createMenuCards(items);
+
+menuCont.insertAdjacentHTML('beforeend', menuMarkup);
+
+
+function createMenuCards(items) {
+    return menuCardsTpl(items)
+}
